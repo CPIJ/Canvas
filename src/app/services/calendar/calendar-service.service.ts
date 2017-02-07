7dd9d8e6cb4c8b2f77049bb901835181a6689771
Coen van Campenhout <coenvc@gmail.com>
2017-02-07 14:10:54 +0100

    expanded the calendar component, it now makes an api call for the tags

--- src/app/services/calendar/calendar-service.service.ts
+++ src/app/services/calendar/calendar-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
-import {CalendarComponent} from '../../components/calendar/calendar.component'; 
+import {CalendarComponent} from '../../components/calendar/calendar.component';  
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map'; 
+import { HttpClient } from '../../services/http/httpClient.service'  
 @Injectable()
 export class CalendarService {
 
-  constructor() { } 
+  constructor(private http: HttpClient) {  
+    
+  } 
+
+  // Type can be teacher or student
+   getTags(type:string){ 
+    return this.http.get('https://api.fhict.nl/calendar/tags/'+type) 
+      .map(res=> <string[]>res.json()); 
+  } 
 
 } 
 
