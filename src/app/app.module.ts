2ac8ffc48729c3e2f094fc950d48e36a3c4b5a6c
Casper Pijnenburg <casper.pijnenburg@student.fontys.nl>
2017-02-07 13:01:36 +0100

    Routing gemaakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,33 +7,53 @@ import { AppComponent } from './app.component';
 
 
 // Custom components
-import { Grade } from '../app/components/grade/grade.component';
-import { Header } from '../app/components/header/header.component'
+import { GradeComponent } from '../app/components/grade/grade.component';
+import { HeaderComponent } from '../app/components/header/header.component'
 import { CalendarComponent } from '../app/components/calendar/calendar.component'
-import { CourseComponent} from './components/course/course.component';
+import { CourseComponent } from './components/course/course.component';
 
 // Data services
 import { GradeService } from '../app/services/grades/grades.service';
 import { HttpClient } from '../app/services/http/httpClient.service';
 
+// Routing
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+    {
+        path: 'courses',
+        component: CourseComponent
+    },
+    {
+        path: 'grades',
+        component: GradeComponent
+    },
+    {
+        path: 'calendar',
+        component: CalendarComponent
+    },
+]
+
+// Module info
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalendarComponent,
-    CourseComponent,
-    Grade,
-    Header
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [
-    GradeService,
-    HttpClient
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CalendarComponent,
+        CourseComponent,
+        GradeComponent,
+        HeaderComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [
+        GradeService,
+        HttpClient
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
