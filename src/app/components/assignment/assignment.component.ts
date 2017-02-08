1238b850177ff203b7d0914f86463b30b1e478a4
Niels Werkman <n.werkman@student.fontys.nl>
2017-02-08 11:54:30 +0100

    course naam bij assignments

--- src/app/components/assignment/assignment.component.ts
+++ src/app/components/assignment/assignment.component.ts
@@ -3,6 +3,9 @@ import {OnInit, OnDestroy} from '@angular/core';
 
 import { Component } from '@angular/core';
 import { AssignmentService } from '../../services/assignment/assignment.service';
+import {CourseComponent} from '../../components/course/course.component';
+import {CourseService} from '../../services/course/course.service';
+
 
 
 
@@ -21,20 +24,25 @@ import { AssignmentService } from '../../services/assignment/assignment.service'
     </div>
 
     `,
-    providers: [AssignmentService]
+    providers: [AssignmentService, CourseService]
 
 })
 
 export class AssignmentComponent {
     title = "dit is de titel";
     private assignments: AssignmentComponent[];
+    private course: CourseComponent;
 
 
-    constructor(assignmentService: AssignmentService, private activatedRoute: ActivatedRoute) {
+    constructor(courseService: CourseService, assignmentService: AssignmentService, private activatedRoute: ActivatedRoute) {
         this.activatedRoute.params.subscribe((params: Params) => {
         let Id = params['id'];
         assignmentService.getAssignments(Id).subscribe(r => this.assignments = r);
-      });
+        courseService.getCourses().subscribe(r => { 
+            this.course = r.filter( c => c.id == Id)[0]
+            //console.log(this.course)
+        }
+        )});
 
         
     }
